bcc5a1e4750ffd6a0489db762fac57cea32b4cc3
Ayah N <ayah.noor0213@gmail.com>
2021-03-11 10:40:57 -0500

    second commit commit

--- src/test/java/TestConnection.java
+++ src/test/java/TestConnection.java
@@ -38,6 +38,36 @@ It also maintain a cursor to point to the row so data at that location can be ac
         try {
             Connection con = DriverManager.getConnection(URL, "hr", "hr");
 
+            // this line is only for dealing with data what to do with it
+            Statement stmnt = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_READ_ONLY);
+
+            // object resultSet(where all results stored all adat that comes from SQL tables)
+            ResultSet rs = stmnt.executeQuery("SELECT * FROM REGIONS");
+
+            // in order to access to ResultSet we need to know we re at
+            // before entering the first row, we re at the cursor resultSet  is at the LOCATION known- AS BEFORE SET
+            // in order to move to next line we need to call next method of resultSet
+            // if order to get any collumn data we need to call methods getString or getDouble...
+
+            rs.next() ;
+            // now we re at 1st row
+            System.out.println("FIRST ROW REGION_NAME is " + rs.getString("REGION_NAME"));
+
+
+            rs.next() ;
+            // now we re at 1st row
+            System.out.println("SECOND ROW REGION_NAME is " + rs.getString("REGION_NAME"));
+
+
+            rs.next() ;
+            // now we re at 1st row
+            System.out.println("THIRD ROW REGION_NAME is " + rs.getString("REGION_NAME"));
+
+
+            rs.next() ;
+            // now we re at 1st row
+            System.out.println("FOURTH ROW REGION_NAME is " + rs.getString("REGION_NAME"));
+
             System.out.println("Connection successfull");
 
         } catch (SQLException e) {
